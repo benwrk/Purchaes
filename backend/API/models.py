e0e11036f64ffb7ae57228d5c59ece110617da73
Norawit <wit543@gmail.com>
2016-09-19 18:36:18 +0700

    make the server runnable

--- backend/API/models.py
+++ backend/API/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class item(MOdels.Model):
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+
+class Item(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=200)
     image_url = models.CharField(max_length=300)
-    description  = model.CharField(Max_length=500)
+    description  = models.CharField(max_length=500)
     tag = models.ManyToManyField(Tag)
-
-class tag(Models.Model):
-    title = models.CharField(max_length=50)
