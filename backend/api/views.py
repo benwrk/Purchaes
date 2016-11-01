31e9d3b34618fd74b28a6455b7949e185a7b247d
Norawit <wit543@gmail.com>
2016-11-01 16:07:33 +0700

    fix bug

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
-def item_search(request):
-    return None
+# from django.shortcuts import render
+# from items.models import Item, Tag
+# from user.model import  User
+# # Create your views here.
+# def item_search(request):
+#     return
