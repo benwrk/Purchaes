e1ee6d0717c785832cd1af722b4b40d6e712d23f
Kwanwan Tantichartkul <pranger54@Kwanwans-MacBook-Pro.local>
2016-11-24 11:24:47 +0700

    add tel number in backend (test)

--- backend/user/models.py
+++ backend/user/models.py
@@ -4,4 +4,6 @@ from django.core.urlresolvers import reverse
 class User(models.Model):
     username = models.CharField(max_length=50,unique=True)
     email = models.TextField(max_length=50,default="")
+    tel = models.TextField(max_length=20)
     bio = models.TextField(max_length=1000)
+
